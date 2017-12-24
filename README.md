38582d07a9653682dbc1b39ebcf41198cefb9152
米田宏 <yonedahiroshi1229@gmail.com>
2017-12-24 13:32:40 +0900

    emacs と zsh の設定ファイル

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotfiles
+
+```
+git clone git@github.com:unamu1229/dotfiles.git  
+```
---
+++ init.el
@@ -0,0 +1,8 @@
+;;対応する括弧を強調表示
+(show-paren-mode t)
+
+;;行番号表示
+(global-linum-mode)
+
+;;Backspace
+(define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
